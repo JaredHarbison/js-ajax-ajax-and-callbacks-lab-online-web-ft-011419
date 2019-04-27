ed669106a392796a7e95e113dccb0980e3b48a09
Jared Harbison <jared.harbison@gmail.com>
2019-04-27 16:13:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 $(document).ready(function (){
 });
+
+
+function displayError() {
+  $('#errors').html(`I'm sorry, there's been an error. Please try again.`);
+}
+
+function searchRepositories() {
+  const searchTerm = $('#searchTerms').value();
+  $.get(`https://api.github.com/search/repositories? q=${searchTerm}`, response => {
+    $("#results").html(displayRepositories(response));
+    }).fail(displayError());
+}
